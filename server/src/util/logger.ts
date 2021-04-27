f89cd83cd8ad8421bca34aefb1c9c5e42b40a300
leegeunhyeok <dev.ghlee@gmail.com>
2021-04-28 00:11:02 +0900

    chore: remove server templates

--- server/src/util/logger.ts
+++ server/src/util/logger.ts
@@ -1,18 +1,21 @@
-import winston from "winston";
+import winston from 'winston';
+
+const logFormat = winston.format.printf((info) => `${info.level}: ${info.message}`);
 
 const options: winston.LoggerOptions = {
-    transports: [
-        new winston.transports.Console({
-            level: process.env.NODE_ENV === "production" ? "error" : "debug"
-        }),
-        new winston.transports.File({ filename: "debug.log", level: "debug" })
-    ]
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), logFormat),
+      level: process.env.NODE_ENV === 'production' ? 'warning' : 'debug',
+    }),
+    new winston.transports.File({ filename: 'debug.log', level: 'debug' }),
+  ],
 };
 
 const logger = winston.createLogger(options);
 
-if (process.env.NODE_ENV !== "production") {
-    logger.debug("Logging initialized at debug level");
+if (process.env.NODE_ENV !== 'production') {
+  logger.debug('Logging initialized at debug level');
 }
 
 export default logger;
